9a28841a5a52f9e79187f303d87286ae9b5aec60
ElviraKondratova <elvirochka_sh@mail.ru>
2025-08-01 13:24:47 +0300

    03_done

---
+++ 03.py
@@ -0,0 +1,41 @@
+#Пузырькова сортировка
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+arr = [64, 34, 25, 12, 22, 33, 5, -6, 11, 90]
+print(bubble_sort(arr))  # [11, 12, 22, 25, 34, 64, 90]
+
+#Сортировка выбором
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+
+arr = [64, 25, 12, 22, 11]
+print(selection_sort(arr))  # [11, 12, 22, 25, 64]
+
+#Сортировка вставками
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+
+arr = [12, 11, -8, 3, 57, 80, 13, 5, 6]
+print(insertion_sort(arr))  # [5, 6, 11, 12, 13]
